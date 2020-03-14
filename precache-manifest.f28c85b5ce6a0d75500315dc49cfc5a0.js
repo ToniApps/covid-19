c6c7866a27555c43751239d1ed459129ae1352d6
Antonio Mora <antoniomorarives@gmail.com>
2020-03-14 18:54:09 +0100

    Updates

---
+++ precache-manifest.f28c85b5ce6a0d75500315dc49cfc5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6691f35f3e0f9a091f9a0b2d7641a3c4",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "e6a42e8f0afe937e140a",
+    "url": "/covid-19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "10bddb14b3df17c1a445",
+    "url": "/covid-19/static/js/2.4601834d.chunk.js"
+  },
+  {
+    "revision": "f28dde22feeb9c1a8865b170c410ee69",
+    "url": "/covid-19/static/js/2.4601834d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a42e8f0afe937e140a",
+    "url": "/covid-19/static/js/main.1ec17a8d.chunk.js"
+  },
+  {
+    "revision": "c75e8770ac86d7f9549a",
+    "url": "/covid-19/static/js/runtime-main.86bf5399.js"
+  }
+]);
